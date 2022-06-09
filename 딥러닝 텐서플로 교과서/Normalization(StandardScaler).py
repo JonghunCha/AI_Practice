b1e4ae46d12977a7cce0bcfce2788d123c7990b0
JonghunCha <jonghun0210.bb@gmail.com>
2022-06-09 18:58:40 +0900

    upload 3 different normalization codes

---
+++ 딥러닝 텐서플로 교과서/Normalization(StandardScaler).py
@@ -0,0 +1,34 @@
+import tensorflow
+import pandas
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+
+#데이터 호출, StandardScaler를 통한 정규화, train, test 분리
+df = pandas.read_csv("./covtype.csv")
+x = df[df.columns[:55]]
+y = df.Cover_Type
+
+norm = x[x.columns[:10]]    #정규화 할 column들 선택
+standard_scaler = preprocessing.StandardScaler().fit(norm)
+x_norm = standard_scaler.transform(norm)
+norm_cols = pandas.DataFrame(x_norm, index=norm.index, columns=norm.columns)
+x.update(norm_cols)
+print(x.head())
+print(x["Elevation"].describe())
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, random_state=10)
+
+#모델 정의
+model = tensorflow.keras.Sequential([
+    tensorflow.keras.layers.Dense(64, activation="relu", input_shape=(x_train.shape[1],)),
+    tensorflow.keras.layers.Dense(64, activation="relu"),
+    tensorflow.keras.layers.Dense(8, activation="softmax")
+])
+
+#모델 컴파일 및 훈련
+model.compile(optimizer=tensorflow.keras.optimizers.Adam(0.01),
+              loss="sparse_categorical_"
+                   "crossentropy",
+              metrics=["accuracy"])
+
+history = model.fit(x_train, y_train, epochs=30, batch_size=64, validation_data=(x_test, y_test))
